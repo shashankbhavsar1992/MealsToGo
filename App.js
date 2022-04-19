7fce6c8ef79f3e6a5967a1874f30d639fc8a3583
Shashank <Shashank@nhancenow.com>
2022-04-19 13:04:30 +0530

    ui changes

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
+const isAndroid = Platform.OS === "android";
+console.log("" + isAndroid ? StatusBar.currentHeight : 0);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: "#fcf5f5",
+          marginTop: isAndroid ? StatusBar.currentHeight : 0,
+        }}
+      >
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "green",
+            alignItems: "center",
+          }}
+        >
+          <Text> Search </Text>
+        </View>
+        <View style={{ padding: 16, backgroundColor: "blue" }}>
+          <Text>
+            Hello this is the list dfwfr ergwer berg b wegw tegset gregre
+            gegsegg w etgew gewrgwr wreg wer ger ger gewg ewgrew gerwg ewrg rew
+            gew gegre gerger ger gergweg rg wr gwr gewrt gew gwegwe gw
+          </Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
